a12ff0f53215a67d31eaf2e183f54cb39224500d
Amirparsa Baghdadi <amirparsab9@gmail.com>
2025-12-19 15:32:38 +0330

    feat: add macro targets type

---
+++ src/types/macro-targets.ts
@@ -0,0 +1,8 @@
+export type MacroTargets = {
+  protein: number; // grams
+  fat: number; // grams
+  carbs: number; // grams
+  sodium: number; // mg
+  sugar: number; // grams
+  fiber: number; // grams
+};
