d54bfaf42b7fbe04a3e322a911dad166a3fdb890
Amirparsa Baghdadi <76398455+amirparsadd@users.noreply.github.com>
2025-12-24 20:10:32 +0330

    fix: update exporting strategy

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,16 @@
-import { ActivityLevel } from './enums/activity-level';
-import { Gender } from './enums/gender';
-import { calculateBMI } from './formulas/bmi';
-import { calculateBMR } from './formulas/bmr';
-import { calculateBodyFat } from './formulas/body-fat';
-import {
+export { Gender } from './enums/gender';
+export { ActivityLevel } from './enums/activity-level';
+
+export { calculateBMI } from './formulas/bmi';
+export { calculateBMR } from './formulas/bmr';
+export { calculateTDEE } from './formulas/tdee';
+export { calculateBodyFat } from './formulas/body-fat';
+export {
   calculateCaloriesForGoal,
   calculateMacrosForCalories,
 } from './formulas/calculate-calories';
-import { calculateHydration } from './formulas/hydration';
-import { calculateLBM } from './formulas/lbm';
-import { calculateBodyWater } from './formulas/tbw';
-import { calculateTDEE } from './formulas/tdee';
-import { estimateVO2Max } from './formulas/vo2max';
-import { calculateWaistToHeightRatio } from './formulas/whtr';
-
-export default {
-  Gender,
-  ActivityLevel,
-  calculateBMI,
-  calculateTDEE,
-  calculateBMR,
-  calculateBodyFat,
-  calculateCaloriesForGoal,
-  calculateMacrosForCalories,
-  calculateHydration,
-  calculateLBM,
-  calculateWaistToHeightRatio,
-  estimateVO2Max,
-  calculateBodyWater,
-};
+export { calculateHydration } from './formulas/hydration';
+export { calculateLBM } from './formulas/lbm';
+export { calculateBodyWater } from './formulas/tbw';
+export { estimateVO2Max } from './formulas/vo2max';
+export { calculateWaistToHeightRatio } from './formulas/whtr';
